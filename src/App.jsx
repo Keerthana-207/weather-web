742f137a841f9b272ef3cee26f10776ca8e51e34
Example Name <name@example.com>
2025-10-07 23:05:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useRef, useState } from 'react'
+import Weather from './Weather'
+// import Navbar from './Navbar'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+    {/* <Navbar inputRef={searchInputRef}/> */}
+    <Weather />
+    </>
+  )
+}
+
+export default App
