1b856eb8882967c27a4134f8d800a0ecfc042e9a
Example Name <name@example.com>
2025-10-09 21:55:36 +0530

    Initial commit

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -142,7 +142,7 @@ function Weather() {
             <p className="local-time">{localTime}</p>
           </div>
           <div className="icon-weather">
-            <p className="temp">{weatherData.temp}{unit === 'metric' ? '°C' : '°F'}</p>
+            <h1 className="temp">{weatherData.temp}{unit === 'metric' ? '°C' : '°F'}</h1>
             <div className="weather-info">
               <img src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} alt="weather-icon" />
               <p className="description">{weatherData.description}</p>
@@ -170,11 +170,36 @@ function Weather() {
             </div>
           </div>
         </div>
-
         {/* Weather Cards (Humidity, Pressure, Sea Level, Wind) */}
         <div className={`weather-cards ${animate ? 'fade-in' : ''}`}>
-          {/* Cards... (unchanged, as per your original structure) */}
-          {/* You can copy them from your current file if you didn't change styles/icons. */}
+          <div className='weather-card'>
+            <h3>Humidity</h3>
+            <div className='content'>
+              <svg xmlns="http://www.w3.org/2000/svg" height="75px" viewBox="0 -960 960 960" width="75px" fill="#fff"><path d="M479-208q16 0 24.5-5.5T512-230q0-11-8.5-17t-25.5-6q-42 0-85.5-26.5T337-373q-2-9-9-14.5t-15-5.5q-11 0-17 8.5t-4 17.5q15 84 71 121.5T479-208Zm1 128q-137 0-228.5-94T160-408q0-100 79.5-217.5T480-880q161 137 240.5 254.5T800-408q0 140-91.5 234T480-80Zm0-60q112 0 186-76.5T740-408q0-79-66.5-179.5T480-800Q353-688 286.5-587.5T220-408q0 115 74 191.5T480-140Zm0-340Z"/></svg>
+              <h2>{weatherData.humidity}</h2>
+            </div>
+          </div>
+          <div className='weather-card'>
+            <h3>Pressure</h3>
+            <div className='content'>
+              <svg xmlns="http://www.w3.org/2000/svg" height="75px" viewBox="0 -960 960 960" width="75px" fill="#fff"><path d="m317-160-42-42 121-121H80v-60h316L275-504l42-42 193 193-193 193Zm326-254L450-607l193-193 42 42-121 121h316v60H564l121 121-42 42Z"/></svg>
+              <h2>{weatherData.pressure}</h2>
+            </div>
+          </div>
+          <div className='weather-card'>
+            <h3>Sea Level</h3>
+            <div className='content'>
+              <svg xmlns="http://www.w3.org/2000/svg" height="75px" viewBox="0 -960 960 960" width="75px" fill="#fff"><path d="M80-251v-60q38 0 61.5-23t68.5-23q45 0 70 27t64 27q39 0 65-27t71-27q45 0 70.5 27t64.5 27q39 0 64.5-27t70.5-27q45 0 68.5 23t61.5 23v60q-47 0-69.5-23T750-297q-38 0-64 27t-71 27q-45 0-71-27t-64-27q-38 0-64.5 27T344-243q-45 0-70.5-27T210-297q-38 0-60.5 23T80-251Zm0-160v-60q38 0 61.5-23t68.5-23q45 0 70 27t64 27q39 0 65-27t71-27q45 0 70.5 27t64.5 27q39 0 64.5-27t70.5-27q45 0 68.5 23t61.5 23v60q-47 0-69.5-23T750-457q-38 0-64 27t-71 27q-45 0-71-27t-64-27q-38 0-64.5 27T344-403q-45 0-70.5-27T210-457q-38 0-60.5 23T80-411Zm0-160v-60q38 0 61.5-23t68.5-23q45 0 70 27t64 27q39 0 65-27t71-27q45 0 70.5 27t64.5 27q39 0 64.5-27t70.5-27q45 0 68.5 23t61.5 23v60q-47 0-69.5-23T750-617q-38 0-64 27t-71 27q-45 0-71-27t-64-27q-38 0-64.5 27T344-563q-45 0-70.5-27T210-617q-38 0-60.5 23T80-571Z"/></svg>
+              <h2>{weatherData.seaLevel}</h2>
+            </div>
+          </div>
+          <div className='weather-card'>
+            <h3>Wind</h3>
+            <div className='content'>
+              <svg xmlns="http://www.w3.org/2000/svg" height="75px" viewBox="0 -960 960 960" width="75px" fill="#fff"><path d="M465-160q-54 0-85.5-28T348-273h68q0 26 11.5 39.5T465-220q27 0 38.5-12t11.5-41q0-29-11.5-42.5T465-329H80v-60h385q54 0 82 28t28 88q0 57-28 85t-82 28ZM80-568v-60h548q37 0 54-17.5t17-58.5q0-41-17-58.5T628-780q-38 0-55 20.5T556-708h-60q0-58 35-95t97-37q61 0 96 35.5T759-704q0 65-35 100.5T628-568H80Zm672 330v-60q35 0 51.5-19.5T820-374q0-38-18.5-55T748-446H80v-60h668q62 0 97 35t35 97q0 64-33 100t-95 36Z"/></svg>
+              <h2>{weatherData.wind}</h2>
+            </div>
+          </div>
         </div>
 
         {/* Forecast */}
